84177e29a2b967fdc710e19d2c2ea124c30754a6
alinscode <160292623+alinscode@users.noreply.github.com>
2024-02-21 09:17:29 +0530

    Update alinstutorial.sh

--- Working with the Google Cloud Console and Cloud Shell/alinstutorial.sh
+++ Working with the Google Cloud Console and Cloud Shell/alinstutorial.sh
@@ -1,8 +1,8 @@
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
 export BUCKET_NAME=$PROJECT_ID
 
-gsutil mb -p $PROJECT_ID gs://$PROJECT_ID-techcps
+gsutil mb -p $PROJECT_ID gs://$PROJECT_ID-alinstutorial
 
-curl -O https://github.com/Techcps/GSP-Short-Trick/blob/main/Cloud%20Storage%3A%20Qwik%20Start%20-%20Cloud%20Console/kitten.png
+curl -O https://github.com/alinscode/Qwiklabs-Solution/blob/main/Working%20with%20the%20Google%20Cloud%20Console%20and%20Cloud%20Shell/kitten.png
 
-gsutil cp kitten.png gs://$PROJECT_ID-techcps/kitten.png
+gsutil cp kitten.png gs://$PROJECT_ID-alinstutorial/kitten.png
