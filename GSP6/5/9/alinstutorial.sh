bbc54c1396a90772baa448da4a7ea29e1e6c48ae
alinscode <160292623+alinscode@users.noreply.github.com>
2024-03-03 03:27:03 +0530

    Update alinstutorial.sh

--- GSP6/5/9/alinstutorial.sh
+++ GSP6/5/9/alinstutorial.sh
@@ -7,16 +7,16 @@ cd ~/monolith-to-microservices./setup.sh
 ./setup.sh
 cd ~/monolith-to-microservices/monolith
 npm start
-gcloud artifacts repositories create monolith-demo --location=us-central1 --repository-format=docker
-gcloud auth configure-docker us-central1-docker.pkg.dev
-gcloud builds submit --tag us-central1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:1.0.0
-gcloud run deploy monolith --image us-central1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:1.0.0 --allow-unauthenticated --region us-central1
-gcloud run deploy monolith --image us-central1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:1.0.0 --allow-unauthenticated --region us-central1 --concurrency 1
-gcloud run deploy monolith --image us-central1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:1.0.0 --allow-unauthenticated --region us-central1 --concurrency 80
+gcloud artifacts repositories create monolith-demo --location=$REGION --repository-format=docker
+gcloud auth configure-docker $REGION-docker.pkg.dev
+gcloud builds submit --tag $REGION-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:1.0.0
+gcloud run deploy monolith --image $REGION-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:1.0.0 --allow-unauthenticated --region $REGION
+gcloud run deploy monolith --image $REGION-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:1.0.0 --allow-unauthenticated --region $REGION --concurrency 1
+gcloud run deploy monolith --image us-central1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:1.0.0 --allow-unauthenticated --region $REGION --concurrency 80
 cd ~/monolith-to-microservices/react-app/src/pages/Home
 mv index.js.new index.js
 cd ~/monolith-to-microservices/react-app
 npm run build:monolith
 cd ~/monolith-to-microservices/monolith
-gcloud builds submit --tag us-central1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:2.0.0
-gcloud run deploy monolith --image us-central1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:2.0.0 --allow-unauthenticated --region us-central1
+gcloud builds submit --tag $REGION-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:2.0.0
+gcloud run deploy monolith --image us-central1-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/monolith-demo/monolith:2.0.0 --allow-unauthenticated --region $REGION
