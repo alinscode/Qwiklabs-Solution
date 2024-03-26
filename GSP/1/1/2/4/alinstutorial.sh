2239f0363cef0ed6b54ee5b3bbfea270ec79be3f
alinscode <160292623+alinscode@users.noreply.github.com>
2024-03-27 04:17:26 +0530

    Update alinstutorial.sh

--- GSP/1/1/2/4/alinstutorial.sh
+++ GSP/1/1/2/4/alinstutorial.sh
@@ -1,31 +1,7 @@
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
 
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
-
-BOLD=`tput bold`
-RESET=`tput sgr0`
 #----------------------------------------------------start--------------------------------------------------#
 
-echo "${YELLOW}${BOLD}
 
-Starting Execution 
-
-
-${RESET}"
 #gcloud auth list
 #gcloud config list project
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
@@ -60,32 +36,16 @@ gcloud scc muteconfigs create muting-pga-findings \
   --filter="category=\"FLOW_LOGS_DISABLED\""
 
 
-echo "${GREEN}${BOLD}
-
-Task 3.1 Completed
-
-${RESET}"
-
 gcloud compute networks create scc-lab-net --subnet-mode=auto
 
-echo "${GREEN}${BOLD}
 
-Task 3.2 Completed
-
-${RESET}"
 
 gcloud compute firewall-rules update default-allow-rdp --source-ranges=35.235.240.0/20
 
 
 gcloud compute firewall-rules update default-allow-ssh --source-ranges=35.235.240.0/20
 
-echo "${GREEN}${BOLD}
 
-Task 3.3 Completed
-
-Lab Completed !!!
-
-${RESET}"
 
 #-----------------------------------------------------end----------------------------------------------------------#
 
